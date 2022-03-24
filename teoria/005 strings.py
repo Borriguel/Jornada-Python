65a5e9d9e8693e216c94223dbb28bbf19bdb7334
Feitam <rodolpho-henrique@hotmail.com.br>
2022-03-24 04:02:31 -0300

    resumo string

---
+++ teoria/005 strings.py
@@ -0,0 +1,10 @@
+# strings são sequências de caracteres e são usadas para gravar informações de textos.
+frase = 'o rato roeu a roupa do rei de roma'
+
+# slice
+print(frase[2])
+print(frase[2:6])
+print(frase[30:])
+
+# comprimento string
+print(len(frase))
