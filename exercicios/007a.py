651d10470ed5ea9e2dee69b04ed428c046099365
Feitam <rodolpho-henrique@hotmail.com.br>
2022-04-03 16:42:11 -0300

    teoria e exercício for

---
+++ exercicios/007a.py
@@ -0,0 +1,7 @@
+# contagem regressiva em segundos de um foguete
+from time import sleep
+for s in range(10,0,-1):
+    print(s)
+    sleep(1)
+print('Lançar!')
+    
---
+++ teoria/007 for.py
@@ -0,0 +1,5 @@
+# utilizados para criar laços de repetições
+
+for c in range(10,0,-1):
+    print(c)
+print('decolar!')
